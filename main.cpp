0aa4c03ac36820fc921a8d4bb37d525f225160c5
janmarius <janmarius_91@hotmail.com>
2019-04-24 15:08:40 +0200

    Organizing and handling settings frames.

--- main.cpp
+++ main.cpp
@@ -341,13 +341,85 @@ static void dataFrameHandler(const unsigned char *data) {
     // Handle payload
 }
 
-static void frameHandler(const unsigned char *data) {
-    // Print length, flag etc.
+static void settingsFrameHandler(const unsigned char *data, size_t length) {
+    size_t indexIdentifier = 9;
+    size_t indexValue = 11;
+    int payloadNumber = 1;
+    for (size_t i = 9; i < length; ++i) {
+        if (i == indexIdentifier) {
+            cout << "\n---" << payloadNumber << "---";
+            payloadNumber++;
+            cout << "\nIdentifier(16):\t\t\t\t";
+            indexIdentifier += 6;
+        }
+        if (i == indexValue) {
+            cout << "\nValue(32):\t\t\t\t\t";
+            indexValue += 6;
+        }
+        printf("%02x", data[i]);
+    }
+}
 
-    // Handle types
+static void frameDefaultPrint(const unsigned char *data) {
+    for (size_t i = 0; i < 9; ++i) {
+        if (i == 0) cout << "Length(24):\t\t\t\t\t";
+        if (i == 3) cout << "\nType(8):\t\t\t\t\t";
+        if (i == 4) cout << "\nFlags(8):\t\t\t\t\t";
+        if (i == 5) cout << "\nStream Identifier(R + 31):\t";
+        printf("%02x", data[i]);
+    }
+}
+
+static void frameHandler(const unsigned char *data, size_t length) {
+    // Print length, flag etc.
     switch (data[3]) {
         case Types::DATA:
-            dataFrameHandler(data);
+            cout << "DATA" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::HEADERS:
+            cout << "HEADER" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::PRIORITY:
+            cout << "PRIORITY" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::RST_STREAM:
+            cout << "RST_STREAM" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::SETTINGS:
+            cout << "SETTINGS" << endl;
+            frameDefaultPrint(data);
+            settingsFrameHandler(data, length);
+            break;
+        case Types::PUSH_PROMISE:
+            cout << "PUSH_PROMISE" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::PING:
+            cout << "PING" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::GOAWAY:
+            cout << "GOAWAY" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::WINDOW_UPDATE:
+            cout << "WINDOW_UPDATE" << endl;
+            frameDefaultPrint(data);
+            break;
+        case Types::CONTINUATION:
+            cout << "CONTINUATION" << endl;
+            frameDefaultPrint(data);
+            break;
+        default:
+            cout << "DEFAULT" << endl;
+            for(size_t i = 0; i < length; ++i) {
+                printf("%02x", data[i]);
+            }
+            break;
     }
 }
 
@@ -357,48 +429,48 @@ static int sessionOnReceived(client_sess_data *clientSessData) {
     size_t length = evbuffer_get_length(in);
     unsigned char *data = evbuffer_pullup(in, -1); // Make whole buffer contiguous
 
-    frameHandler(data);
+    frameHandler(data, length);
 
-    cout << "Length: " << length << endl;
-
-    cout << "Length: ";
-    for (size_t i = 0; i < 3 ; ++i) {
-        printf("%02x", data[i]);
-    }
-
-    cout << "\nType: ";
-    printf("%02x", data[3]);
-
-    cout << "\nFlags: ";
-    printf("%02x", data[4]);
-
-    cout << "\nStream Identifier: ";
-    for (size_t i = 5; i < 9 ; ++i) {
-        printf("%02x", data[i]);
-    }
-
-    if (length >= 9) {
-        cout << "\nPayload: ";
-        if (data[3] == Types::SETTINGS && data[4] == 0x00) {
-            for (size_t i = 9; i < length; i += 6) {
-                cout << endl;
-                for (size_t j = i; j < i + 2; ++j) {
-                    printf("%02x", data[j]);
-                }
-                cout << " : ";
-                for (size_t j = i + 2; j < i + 4; ++j) {
-                    printf("%02x", data[j]);
-                }
-            }
-            cout << "\nSending 'ACK' SETTINGS FRAME." << endl;
-            sendConnectionHeader(clientSessData);
-
-            char data[] = { 0x00, 0x00, 0x00, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00 };
-            bufferevent_write(clientSessData->bufferEvent, data, 9);
-
-        }
-
-    }
+//    cout << "Length: " << length << endl;
+//
+//    cout << "Length: ";
+//    for (size_t i = 0; i < 3 ; ++i) {
+//        printf("%02x", data[i]);
+//    }
+//
+//    cout << "\nType: ";
+//    printf("%02x", data[3]);
+//
+//    cout << "\nFlags: ";
+//    printf("%02x", data[4]);
+//
+//    cout << "\nStream Identifier: ";
+//    for (size_t i = 5; i < 9 ; ++i) {
+//        printf("%02x", data[i]);
+//    }
+//
+//    if (length >= 9) {
+//        cout << "\nPayload: ";
+//        if (data[3] == Types::SETTINGS && data[4] == 0x00) {
+//            for (size_t i = 9; i < length; i += 6) {
+//                cout << endl;
+//                for (size_t j = i; j < i + 2; ++j) {
+//                    printf("%02x", data[j]);
+//                }
+//                cout << " : ";
+//                for (size_t j = i + 2; j < i + 4; ++j) {
+//                    printf("%02x", data[j]);
+//                }
+//            }
+//            cout << "\nSending 'ACK' SETTINGS FRAME." << endl;
+//            sendConnectionHeader(clientSessData);
+//
+//            char data[] = { 0x00, 0x00, 0x00, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00 };
+//            bufferevent_write(clientSessData->bufferEvent, data, 9);
+//
+//        }
+//
+//    }
 
     /*
     if (data[3] == 0x04 && data[4] == 0x0) {
