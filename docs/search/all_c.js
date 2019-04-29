16fa7b183c47f02453d75a5fe60fdfa99aac0102
Magne Worren <magneworren@gmail.com>
2019-04-29 20:05:22 +0200

    Updated docs

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['readcallback',['readCallback',['../classh2__callbacks.html#a75a7b6da7c86b72831ed5f034d3c6007',1,'h2_callbacks']]],
+  ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
   ['refused_5fstream',['REFUSED_STREAM',['../h2__enums_8hpp.html#a3878e89dc5c88d823f4f923ef90dbcb3aca5f766ef1730ffb588a8a8d7366a0b6',1,'h2_enums.hpp']]],
   ['resolvepath',['resolvePath',['../classh2__utils.html#a12b0fd6caaf10b8b2680c95f969da86d',1,'h2_utils']]],
   ['routes',['routes',['../structApplicationContext.html#a7545bf0fd2df02ee66320b7e24626817',1,'ApplicationContext']]],
