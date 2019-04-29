5f282b6629b6527d07a4de342bb26191af165bf4
Orjan Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-04-29 22:31:22 +0200

    Include dist: xenial in travis.yml

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install g++ make binutils autoconf automake autotools-dev cmake libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libboost-dev -y
 
+
 # Download, build, and install nghttp2
 git clone https://github.com/nghttp2/nghttp2.git && cd nghttp2
 git submodule update --init
