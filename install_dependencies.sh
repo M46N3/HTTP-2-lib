16a034bbdd2c6ff28ff299cb09fe95e8520af9f1
Orjan Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-04-29 21:56:34 +0200

    Testing travic.yml

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install g++ make binutils autoconf automake autotools-dev cmake libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev -y
+sudo apt-get install g++ make binutils autoconf automake autotools-dev cmake libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libboost-all-dev -y
 
 # Download, build, and install nghttp2
 git clone https://github.com/nghttp2/nghttp2.git && cd nghttp2
@@ -12,4 +12,8 @@ autoconf
 ./configure --enable-apps
 make && sudo make install
 
-# Download, build, and install nghttp2
+# Download, build, and install boost
+#wget https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.zip
+#unzip boost_1_70_0.zip && cd boost_1_70_0
+#chmod +x bootstrap.sh
+#./
