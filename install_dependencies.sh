bf81eb1f7fe11533d84613aa9875286a849eae30
Orjan Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-04-29 23:05:37 +0200

    Building boost from source without ./B2

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install g++ make binutils autoconf automake autotools-dev cmake libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libboost-dev -y
+sudo apt-get install g++ make binutils autoconf automake autotools-dev cmake libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev -y
 
 
 # Download, build, and install nghttp2
@@ -14,9 +14,7 @@ autoconf
 make && sudo make install
 
 # Download, build, and install boost
-wget -O boost_1_68_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.68.0/boost_1_68_0.tar.gz/download
-tar xzvf boost_1_68_0.tar.gz && cd boost_1_68_0
-chmod +x bootstrap.sh
-./bootstrap.sh --prefix=/usr/
-./b2
-sudo ./b2 install
+wget -O boost_1_68_0.tar.bz2 https://sourceforge.net/projects/boost/files/boost/1.68.0/boost_1_68_0.tar.bz2/download
+tar xzvf boost_1_68_0.tar.bz2
+cp -r boost_1_68_0 /usr/local/
+rm -r boost_1_68_0
