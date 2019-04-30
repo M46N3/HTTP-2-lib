39239f96093fcae7ae9eceadb1ba0659911661e5
Orjan Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-04-30 02:51:27 +0200

    #10035

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,18 +5,18 @@ sudo apt-get install make binutils autoconf automake autotools-dev cmake libtool
 
 
 # Download, build, and install nghttp2
-git clone https://github.com/nghttp2/nghttp2.git && cd nghttp2
-git submodule update --init
-autoreconf -i
-automake
-autoconf
-./configure --prefix=/usr \
-            --disable-static \
-            --enable-lib-only \
-            --docdir=/usr/share/doc/nghttp2-1.48.0
-make
-sudo make install
-ls /usr/inlcude
+#git clone https://github.com/nghttp2/nghttp2.git && cd nghttp2
+#git submodule update --init
+#autoreconf -i
+#automake
+#autoconf
+#./configure --prefix=/usr \
+#            --disable-static \
+#            --enable-lib-only \
+#            --docdir=/usr/share/doc/nghttp2-1.48.0
+#make
+#sudo make install
+#ls /usr/inlcude
 
 # Download, build, and install boost
 wget -O boost_1_68_0.tar.bz2 https://sourceforge.net/projects/boost/files/boost/1.68.0/boost_1_68_0.tar.bz2/download
